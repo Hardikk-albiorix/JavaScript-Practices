f6a8fac83df50906b6028cb87d3898816a1f937b
Hardikk-albiorix <hardikk.albiorix@gmail.com>
2023-04-20 17:41:35 +0530

    feat(arithmatic-operation): implement arithmation operation program

--- arithmatic-operation.js
+++ arithmatic-operation.js
@@ -1,3 +1,4 @@
+//A program for addition, substraction, multiplication and division of two numbers 
 const x = 20;
 const y = 10;
 
@@ -12,3 +13,15 @@ console.log("Multiplication of x and y :" + z);
 
 var z = x / y;
 console.log("Division of x and y :" + z);
+
+//A program for square of two numbers
+
+console.log("Square of x :" + x**2);
+
+//A program for concatenate two strings
+
+const a = "Hello";
+const b = "World!";
+
+let c = a + " " + b;
+console.log("result :" + c);
