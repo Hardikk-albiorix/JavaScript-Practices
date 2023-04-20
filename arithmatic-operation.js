623b715aeee6e12c23223ec685014d226b3f015e
Hardikk-albiorix <hardikk.albiorix@gmail.com>
2023-04-20 17:28:43 +0530

    feat(arithmatic-operation): implement arithmatic operation

---
+++ arithmatic-operation.js
@@ -0,0 +1,14 @@
+const x = 20;
+const y = 10;
+
+var z = x + y;
+console.log("Addition of x and y :" + z);
+
+var z = x - y;
+console.log("Substraction of x and y :" + z);
+
+var z = x * y;
+console.log("Multiplication of x and y :" + z);
+
+var z = x / y;
+console.log("Division of x and y :" + z);
