ab0077a4b11c3e7bbb8617aa04f37cb7cde0f18e
Hardikk-albiorix <hardikk.albiorix@gmail.com>
2023-04-24 17:23:24 +0530

    feat(arithmatic-operator): implement arithmatic operator example

--- arithmatic-operation.js
+++ arithmatic-operation.js
@@ -1,22 +1,23 @@
-//A program for addition, substraction, multiplication and division of two numbers 
+//A program for addition, substraction, multiplication and division of two numbers
 const x = 20;
 const y = 10;
+var z;
 
-var z = x + y;
+z = x + y;
 console.log("Addition of x and y :" + z);
 
-var z = x - y;
+z = x - y;
 console.log("Substraction of x and y :" + z);
 
-var z = x * y;
+z = x * y;
 console.log("Multiplication of x and y :" + z);
 
-var z = x / y;
+z = x / y;
 console.log("Division of x and y :" + z);
 
 //A program for square of two numbers
 
-console.log("Square of x :" + x**2);
+console.log("Square of x :" + x ** 2);
 
 //A program for concatenate two strings
 
