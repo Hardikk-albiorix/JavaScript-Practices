b427e8e7923d282bf446385b4a9abfa4057407b2
Hardikk-albiorix <hardikk.albiorix@gmail.com>
2023-04-24 17:37:06 +0530

    feat(number): implement reverse number example

---
+++ reverse-number.js
@@ -0,0 +1,14 @@
+let remainder,reverse_number = 0;
+
+function reverseNumber(num) {
+  while (num != 0) {
+    remainder = num % 10;
+    reverse_number = reverse_number * 10 + remainder;
+    num = parseInt(num / 10);
+  }
+}
+
+reverseNumber(-526);
+console.log("Reversed Number :" + reverse_number);
+
+
