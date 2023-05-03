b5c2710837ed11776a7b09fee0cb8c3e2c4a4129
Hardikk-albiorix <hardikk.albiorix@gmail.com>
2023-05-03 13:48:23 +0530

    fix(string): code update

--- longest_word.js
+++ longest_word.js
@@ -1,9 +1,27 @@
+// Write a program for finding the longest words from the given string
+
+//SOLUTION : 1
 function findLongestWord(str) {
-  const str_arr = str.split(' ');
-  let max = str_arr[0].length;
-  str_arr.map((word) => (max = Math.max(max, word.length)));
-  longest_word = str_arr.filter(word => word.length == max);
+  const strArr = str.split(' ');
+  let max = strArr[0].length;
+  strArr.map((word) => (max = Math.max(max, word.length)));
+  longest_word = strArr.filter(word => word.length == max);
   return longest_word;
 }
-console.log(findLongestWord("My name is Hardik Kanjariya Kanjariya abcdefghi"));
- 
+findLongestWord("My name is Hardik Kanjariya Kanjariya abcdefghi");
+
+//SOLUTION : 2
+function findLongestWord(str) {
+  let maxStr = "";
+  let maxStrArr = [];
+  for (let item of str.split(" ")) {
+      if (item.length > maxStr.length) {
+          maxStr = item;
+        } else if (item.length === maxStr.length) {
+            maxStrArr.push(item);
+        }
+  }
+  console.log(`${maxStr},${maxStrArr}`);
+}
+findLongestWord("My name is Hardik Kanjariya Kanjariya abcdefghi");
+
